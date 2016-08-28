b4dd5f50e9f2d28715a8c525b3c0416a53e4b9f9
Joopkins <jobndingdier@gmail.com>
2016-08-28 11:44:02 -0400

    feat: Set up tableViews

---
+++ DreamLister/ItemCell.swift
@@ -0,0 +1,18 @@
+//
+//  ItemCell.swift
+//  DreamLister
+//
+//  Created by Richard Bryant on 8/28/16.
+//  Copyright © 2016 Joopkins. All rights reserved.
+//
+
+import UIKit
+
+class ItemCell: UITableViewCell {
+
+    @IBOutlet weak var thumb: UIImageView!
+    @IBOutlet weak var itemTitle: UILabel!
+    @IBOutlet weak var price: UILabel!
+    @IBOutlet weak var details: UILabel!
+
+}
